9560c8c4ef6627d8067dc20b876545e97723aec3
Adam Finlayson <amf@adamfinlayson.com>
2023-12-29 18:50:01 -0600

    chore: remove: old vue 2 code

--- Vue2-Vuetify-Vuex/store/mutations.js
+++
@@ -1,9 +0,0 @@
-// export const MUTATE = {
-//   MUTATION_NAME: "_mutationName",
-// };
-
-// export default {
-//   [MUTATE.MUTATION_NAME](state, player) {
-//     state.player = player;
-//   },
-// };
