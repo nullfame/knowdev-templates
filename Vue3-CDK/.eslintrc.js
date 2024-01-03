5f7b5a195f17315ee856c1fb7ac15bf86d6e94d8
Adam Finlayson <amf@adamfinlayson.com>
2024-01-02 18:19:11 -0600

    feat: vitest

--- Vue3-CDK/.eslintrc.js
+++ Vue3-CDK/.eslintrc.js
@@ -8,4 +8,15 @@ module.exports = {
     "eslint:recommended",
     "plugin:prettier-vue/recommended",
   ],
+  overrides: [
+    {
+      files: ["**/*.spec.js", "**/*.test.js"],
+      plugins: ["vitest"],
+      extends: ["plugin:vitest/recommended"],
+      rules: {
+        "vitest/no-focused-tests": "error",
+        "vitest/no-disabled-tests": "warn",
+      },
+    },
+  ],
 };
